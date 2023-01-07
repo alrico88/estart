536e70ba324c1e808a76beeff8a8eff7b291d5da
Alberto Rico <alrico88@gmail.com>
2023-01-07 12:57:09 +0100

    fix: icons names as lowercase

--- utils/weather-conditions.ts
+++ utils/weather-conditions.ts
@@ -4,189 +4,189 @@ const conditions = [
   {
     code: 1,
     icon: {
-      day: "wi:day-Cloudy",
+      day: "wi:day-cloudy",
       night: "wi:night-alt-cloudy",
     },
   },
   {
     code: 2,
     icon: {
-      day: "wi:day-Cloudy",
+      day: "wi:day-cloudy",
       night: "wi:night-alt-cloudy",
     },
   },
   {
     code: 3,
     icon: {
-      day: "wi:day-Cloudy",
+      day: "wi:day-cloudy",
       night: "wi:night-alt-cloudy",
     },
   },
   {
     code: 45,
     icon: {
-      day: "wi:day-Fog",
+      day: "wi:day-fog",
       night: "wi:night-fog",
     },
   },
   {
     code: 48,
     icon: {
-      day: "wi:day-Fog",
+      day: "wi:day-fog",
       night: "wi:night-fog",
     },
   },
   {
     code: 51,
     icon: {
-      day: "wi:day-Sprinkle",
+      day: "wi:day-sprinkle",
       night: "wi:night-alt-sprinkle",
     },
   },
   {
     code: 53,
     icon: {
-      day: "wi:day-Sprinkle",
+      day: "wi:day-sprinkle",
       night: "wi:night-alt-sprinkle",
     },
   },
   {
     code: 55,
     icon: {
-      day: "wi:day-Sprinkle",
+      day: "wi:day-sprinkle",
       night: "wi:night-alt-sprinkle",
     },
   },
   {
     code: 56,
     icon: {
-      day: "wi:day-Sleet",
+      day: "wi:day-sleet",
       night: "wi:night-alt-sleet",
     },
   },
   {
     code: 57,
     icon: {
-      day: "wi:day-Sleet",
+      day: "wi:day-sleet",
       night: "wi:night-alt-sleet",
     },
   },
   {
     code: 61,
     icon: {
-      day: "wi:day-Showers",
+      day: "wi:day-showers",
       night: "wi:night-alt-showers",
     },
   },
   {
     code: 63,
     icon: {
-      day: "wi:day-Showers",
+      day: "wi:day-showers",
       night: "wi:night-alt-showers",
     },
   },
   {
     code: 65,
     icon: {
-      day: "wi:day-Showers",
+      day: "wi:day-showers",
       night: "wi:night-alt-showers",
     },
   },
   {
     code: 66,
     icon: {
-      day: "wi:day-Sleet",
+      day: "wi:day-sleet",
       night: "wi:night-alt-sleet",
     },
   },
   {
     code: 67,
     icon: {
-      day: "wi:day-Sleet",
+      day: "wi:day-sleet",
       night: "wi:night-alt-sleet",
     },
   },
   {
     code: 71,
     icon: {
-      day: "wi:day-Snow",
+      day: "wi:day-snow",
       night: "wi:night-alt-snow",
     },
   },
   {
     code: 73,
     icon: {
-      day: "wi:day-Snow",
+      day: "wi:day-snow",
       night: "wi:night-alt-snow",
     },
   },
   {
     code: 75,
     icon: {
-      day: "wi:day-Snow",
+      day: "wi:day-snow",
       night: "wi:night-alt-snow",
     },
   },
   {
     code: 77,
     icon: {
-      day: "wi:day-Snow",
+      day: "wi:day-snow",
       night: "wi:night-alt-snow",
     },
   },
   {
     code: 80,
     icon: {
-      day: "wi:day-Snow",
+      day: "wi:day-snow",
       night: "wi:night-alt-snow",
     },
   },
   {
     code: 81,
     icon: {
-      day: "wi:day-Snow",
+      day: "wi:day-snow",
       night: "wi:night-alt-snow",
     },
   },
   {
     code: 82,
     icon: {
-      day: "wi:day-Snow",
+      day: "wi:day-snow",
       night: "wi:night-alt-snow",
     },
   },
   {
     code: 85,
     icon: {
-      day: "wi:day-Snow",
+      day: "wi:day-snow",
       night: "wi:night-alt-snow",
     },
   },
   {
     code: 86,
     icon: {
-      day: "wi:day-Snow",
+      day: "wi:day-snow",
       night: "wi:night-alt-snow",
     },
   },
   {
     code: 95,
     icon: {
-      day: "wi:day-Thunderstorm",
+      day: "wi:day-thunderstorm",
       night: "wi:night-alt-thunderstorm",
     },
   },
   {
     code: 96,
     icon: {
-      day: "wi:day-Thunderstorm",
+      day: "wi:day-thunderstorm",
       night: "wi:night-alt-thunderstorm",
     },
   },
   {
     code: 99,
     icon: {
-      day: "wi:day-Thunderstorm",
+      day: "wi:day-thunderstorm",
       night: "wi:night-alt-thunderstorm",
     },
   },
